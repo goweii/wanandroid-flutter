3d86bc23effdc32adc5020e298606c35cd614240
goweii <goweii@163.com>
2022-01-15 20:20:24 +0800

    修复分享文章bug

--- lib/module/share/share_article_view_model.dart
+++ lib/module/share/share_article_view_model.dart
@@ -1,20 +1,11 @@
 import 'package:wanandroid/api/wan/wan_apis.dart';
-import 'package:wanandroid/env/http/api.dart';
-import 'package:wanandroid/env/l10n/generated/l10n.dart';
 import 'package:wanandroid/env/mvvm/view_model.dart';
 
 class ShareArticleViewModel extends ViewModel {
-  Future<dynamic> shareArticle({
+  Future<String?> shareArticle({
     required String title,
     required String link,
   }) async {
-    try {
-      await WanApis.shareArticle(title: title, link: link);
-      return null;
-    } on ApiException catch (e) {
-      return e.msg ?? Strings.current.unknown_error;
-    } catch (_) {
-      return Strings.current.unknown_error;
-    }
+    await WanApis.shareArticle(title: title, link: link);
   }
 }
